4352b3c9821c6be882a03fd3e0b22afc40ea1e8d
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-03-27 21:09:34 +0100

    simpler to have everything in one file for now

--- marmotcore.go
+++ marmotcore.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -29,6 +31,41 @@ func (mc MarmotcoreClient) url() string {
 
 func (mc MarmotcoreClient) getRequest(path string) (resp *http.Response, err error) {
 	return Client.Get(mc.url() + path)
+}
+
+type Node struct {
+	UserId       string `json:"user_id"`
+	CreatedTime  int    `json:"created_time"`
+	NodeId       string `json:"node_id"`
+	Region       string `json:"region"`
+	InstanceType string `json:"instance_type"`
+	ChiaVersion  string `json:"chia_version"`
+	Network      string `json:"network"`
+	State        string `json:"state"`
+	Deleted      bool   `json:"deleted"`
+	DeletedTime  int    `json:"deleted_time,omitempty"`
+}
+
+type Nodes struct {
+	Nodes []Node `json:"nodes"`
+}
+
+func (mc MarmotcoreClient) GetNodes() (Nodes, error) {
+	var nodes Nodes
+
+	resp, err := mc.getRequest("/nodes")
+
+	if err != nil {
+		fmt.Printf("Error %s", err)
+		return nodes, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	json.Unmarshal(body, &nodes)
+
+	return nodes, nil
 }
 
 func main() {
